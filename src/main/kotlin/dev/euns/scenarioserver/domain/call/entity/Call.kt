575901215e4b867fe01718524b7c78c622d1e3a6
SangEun Lee <dev.max@icloud.com>
2025-03-08 21:51:35 +0900

    Fetch files

--- src/main/kotlin/dev/euns/scenarioserver/domain/call/entity/Call.kt
+++ src/main/kotlin/dev/euns/scenarioserver/domain/call/entity/Call.kt
@@ -10,9 +10,13 @@
 //    @Id
 //    @GeneratedValue(strategy = GenerationType.IDENTITY)
 //    val id: Long = 0,
+//
 //    val scenario_id: Long,
+//
 //    val thread_id: String,
+//
 //    val assistant_id: String,
+//
 //    val created_at: LocalDateTime = LocalDateTime.now(),
 //
-//    )
+//)
